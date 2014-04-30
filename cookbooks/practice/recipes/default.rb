65492a6d136cdf8a61950b3762a497bcb2e55d58
Roshan <roshan4074@gmail.com>
2014-04-30 14:57:45 +0530

    practice recipe changed

--- cookbooks/practice/recipes/default.rb
+++ cookbooks/practice/recipes/default.rb
@@ -7,6 +7,10 @@
 # All rights reserved - Do Not Redistribute
 #
 
+package "apache2" do
+  action install
+end
+
 template "/etc/yum.conf" do
   source "yum.conf"
 end
@@ -15,3 +19,6 @@ template "/etc/resolve.conf" do
   source "resolve.conf"
 end
 
+service "apache2" do
+  action [:start, enabled]
+end
