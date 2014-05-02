c3d147db9d8e07682f9f52603a6fc3abd20f8311
Roshan <roshan4074@gmail.com>
2014-05-02 10:44:46 +0530

    removed practice cookbook added role webserver

--- roles/webserver.rb
+++ roles/webserver.rb
@@ -5,7 +5,7 @@ all_env = [
   "recipe[php]",
   "recipe[php::module_mysql]",
   "recipe[apache2]",
-  "recipe[apache2::mod_php5",
+  "recipe[apache2::mod_php5]",
   "recipe[apache2::mod_rewrite]",
 ]
 
